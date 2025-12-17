e1f3969d622b64b8e48945dc9de769cf9cc75393
cntooturk <t.aha.a@hotmail.com>
2025-12-17 23:17:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -376,7 +376,7 @@ if st.session_state.aktif_arama and not st.session_state.takip_modu:
         
         if temiz_data:
             ham_toplam = sum(b.get('gunlukYolcu', 0) for b in temiz_data)
-            kalibre_toplam = int(ham_toplam * 1.215)
+            kalibre_toplam = int(ham_toplam * 1.22)
             
             c_toplam, c_arac = st.columns(2)
             c_toplam.markdown(f"""
@@ -408,7 +408,7 @@ if st.session_state.aktif_arama and not st.session_state.takip_modu:
                 c2.write(f"{bus['hiz']}")
                 
                 h_yolcu = bus.get('gunlukYolcu', 0) or 0
-                k_yolcu = int(h_yolcu * 1.215)
+                k_yolcu = int(h_yolcu * 1.22)
                 c3.write(f"{k_yolcu}")
                 
                 maps = google_maps_link(bus['enlem'], bus['boylam'])
@@ -494,7 +494,7 @@ if st.session_state.takip_modu and st.session_state.secilen_plaka:
     
     ham_anlik = arac.get('seferYolcu')
     ham_toplam = arac.get('gunlukYolcu', 0) or 0
-    kalibre_toplam = int(ham_toplam * 1.215) # %21.5 Kalibrasyon
+    kalibre_toplam = int(ham_toplam * 1.22) # %21.5 Kalibrasyon
 
     c1, c2, c3, c4 = st.columns(4)
     c1.markdown(f"""<div class="metric-card"><div class="metric-title">HAT</div><div class="metric-value" style="color:#ff4b4b;">{hat_no}</div></div>""", unsafe_allow_html=True)
@@ -530,5 +530,6 @@ if st.session_state.takip_modu and st.session_state.secilen_plaka:
 if st.session_state.aktif_arama:
     time.sleep(20)
     st.rerun()
+
 
 
